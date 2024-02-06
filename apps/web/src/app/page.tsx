bfcd3cee1e9da639d16b9a9a8504d1f6f8f570c4
turboh <805luca805@gmail.com>
2024-02-06 22:16:46 +0100

    test: testing if dev branch deployment works

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Page(): JSX.Element {
   return (
     <div className="flex items-center flex-col">
       <Test />
-      <Button>Hello!</Button>
+      <Button>Hello!!!!!</Button>
     </div>
   );
 }
